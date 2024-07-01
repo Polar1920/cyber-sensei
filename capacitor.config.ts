71f9634acbd05abf0f4e792a1737b9d674ba3f35
Polar <dmpp1920@gmail.com>
2024-07-01 17:20:27 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Cyber Sensei',
+  webDir: 'dist'
+};
+
+export default config;
